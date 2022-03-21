b75f8074fbf1efee0406792eefd40804d38322dc
Phan Long <>
2022-03-21 17:34:22 +0700

    banner destop: 100%

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
-$(document).ready(() =>{
-    $('.owl-carousel-banner').owlCarousel({
+$(document).ready(() => {
+    const owlCarouselBanner = $('.owl-carousel-banner')
+    owlCarouselBanner.owlCarousel({
         items: 1,
-        loop: true
+        loop: true,
+        nav: false,
+        dots: false
     });
+    $('.custom-next-btn-banner').click(function () {
+        owlCarouselBanner.trigger('next.owl.carousel');
+    })
+    $('.custom-prev-btn-banner').click(function () {
+        owlCarouselBanner.trigger('prev.owl.carousel', [300]);
+    })
 })
