e52730b1dd7d4d47c675121f549590af3d2cfd33
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-03-22 16:30:50 +0700

    feat: our customers - 100%

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,33 @@ $(document).ready(() => {
         owlCarouselBanner.trigger('prev.owl.carousel', [300]);
     })
 })
+
+
+$(document).ready(() => {
+    const owlCarouselOurCustomers = $('.owl-carousel-our-customers')
+    owlCarouselOurCustomers.owlCarousel({
+        items: 3,
+        loop: true,
+        nav: false,
+        margin: 50,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+    
+    $('.custom-next-btn-banner').click(function () {
+        owlCarouselOurCustomers.trigger('next.owl.carousel');
+    })
+    $('.custom-prev-btn-banner').click(function () {
+        owlCarouselOurCustomers.trigger('prev.owl.carousel', [300]);
+    })
+})
