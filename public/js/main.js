4d07d0067b35e8f1c6a3b9bedb04438d2f0bcf1f
Phan Long <>
2022-03-22 11:22:06 +0700

    certificate

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     const owlCarouselBanner = $('.owl-carousel-banner')
+    const owlCarouselCertificate = $('.owl-carousel-certificate')
     owlCarouselBanner.owlCarousel({
         items: 1,
         loop: true,
@@ -11,5 +12,26 @@ $(document).ready(() => {
     })
     $('.custom-prev-btn-banner').click(function () {
         owlCarouselBanner.trigger('prev.owl.carousel', [300]);
+    })
+
+    owlCarouselCertificate.owlCarousel({
+        // stagePadding: 50,
+        loop: true,
+        nav: false,
+        dots: false,
+        responsive: {
+            1440:{
+                items:4,
+            },
+            1024: {
+                items: 3,
+            },
+            768: {
+                items: 2,
+            },
+            425: {
+                items: 1,
+            }
+        }
     })
 })
