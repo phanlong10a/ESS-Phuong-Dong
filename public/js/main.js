5e376b49a2d6f0ece9f6210b523e40d90df3c74c
Tuyen Pham <tuyen197.it@gmail.com>
2022-03-22 17:27:24 +0700

    style: create, responsive OUR ACTIVITIES

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,40 @@
 $(document).ready(() => {
     const owlCarouselBanner = $('.owl-carousel-banner')
     const owlCarouselCertificate = $('.owl-carousel-certificate')
+    const ourActivitiesCarousel = $('.our-activities-carousel')
+
     owlCarouselBanner.owlCarousel({
         items: 1,
         loop: true,
         nav: false,
         dots: false
     });
+    // 
+    ourActivitiesCarousel.owlCarousel({
+        items: 3,
+        loop: true,
+        nav: false,
+        dots: false,
+        responsive: {
+            1024: {
+                items: 3,
+            },
+            768: {
+                items: 2,
+            },
+            425: {
+                items: 1,
+            }
+        }
+    });
+    // custom btn owl our-activities
+    $('.custom-next-btn-activities').click(function () {
+        ourActivitiesCarousel.trigger('next.owl.carousel');
+    })
+    $('.custom-prev-btn-activities').click(function () {
+        ourActivitiesCarousel.trigger('prev.owl.carousel', [300]);
+    })
+    // 
     $('.custom-next-btn-banner').click(function () {
         owlCarouselBanner.trigger('next.owl.carousel');
     })
