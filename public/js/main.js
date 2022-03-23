8383f72f0ea11198964003bbd9edc4d57658a5ac
Phan Long <>
2022-03-23 11:03:44 +0700

    merge code

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,8 @@ $(document).ready(() => {
     const ourActivitiesCarousel = $('.our-activities-carousel')
     const owlCarouselOurCustomers = $('.owl-carousel-our-customers')
     const owlJobVacancies = $('.owl-job-vacancies')
-    const mainNav = $('#main-nav')
+    const owlCarouselOurClients = $('.owl-carousel-our-clients')
+
     owlCarouselBanner.owlCarousel({
         items: 1,
         loop: true,
@@ -17,6 +18,9 @@ $(document).ready(() => {
     $('.custom-prev-btn-banner').click(function () {
         owlCarouselBanner.trigger('prev.owl.carousel', [300]);
     })
+
+
+
     owlJobVacancies.owlCarousel({
         loop: true,
         nav: false,
@@ -34,7 +38,7 @@ $(document).ready(() => {
             },
             425: {
                 items: 1,
-            }
+            }, 0: { items: 1 }
         }
     })
     $('.custom-next-btn-job-vacancies').click(function () {
@@ -43,6 +47,38 @@ $(document).ready(() => {
     $('.custom-prev-btn-job-vacancies').click(function () {
         owlJobVacancies.trigger('prev.owl.carousel', [300]);
     })
+
+
+
+    owlCarouselOurClients.owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        margin: 15,
+        responsive: {
+            1440: {
+                items: 4,
+            },
+            1024: {
+                items: 3,
+            },
+            768: {
+                items: 2,
+            },
+            425: {
+                items: 1,
+            }, 0: { items: 1 },
+        }
+    })
+
+    $('.custom-next-btn-our-customers').click(function () {
+        owlCarouselOurClients.trigger('next.owl.carousel');
+    })
+    $('.custom-prev-btn-our-customers').click(function () {
+        owlCarouselOurClients.trigger('prev.owl.carousel', [300]);
+    })
+
+
     owlCarouselOurCustomers.owlCarousel({
         loop: true,
         nav: false,
@@ -60,16 +96,23 @@ $(document).ready(() => {
             },
             425: {
                 items: 1,
-            }
+            }, 0: { items: 1 }
         }
     })
-    
+
     $('.custom-next-btn-our-customers').click(function () {
         owlCarouselOurCustomers.trigger('next.owl.carousel');
     })
     $('.custom-prev-btn-our-customers').click(function () {
         owlCarouselOurCustomers.trigger('prev.owl.carousel', [300]);
     })
+
+
+
+
+
+
+
     owlCarouselCertificate.owlCarousel({
         // stagePadding: 50,
         loop: true,
@@ -87,7 +130,7 @@ $(document).ready(() => {
             },
             425: {
                 items: 1,
-            }
+            }, 0: { items: 1 }
         }
     })
     ourActivitiesCarousel.owlCarousel({
@@ -97,8 +140,8 @@ $(document).ready(() => {
         dots: false,
         margin: 15,
         responsive: {
-            1440:{
-                items:4
+            1440: {
+                items: 4
             },
             1024: {
                 items: 3,
@@ -119,14 +162,20 @@ $(document).ready(() => {
         ourActivitiesCarousel.trigger('prev.owl.carousel', [300]);
     })
 
+
+})
+
+$(document).ready(() => {
+    const mainNav = $('#main-nav');
+
     window.addEventListener("scroll", (event) => {
         let scroll = this.scrollY;
-        if(scroll > screen.height){
-            if(!mainNav.hasClass('background-blue')){
+        if (scroll > screen.height) {
+            if (!mainNav.hasClass('background-blue')) {
                 mainNav.addClass('background-blue')
             }
-        }else{
-            if(mainNav.hasClass('background-blue')){
+        } else {
+            if (mainNav.hasClass('background-blue')) {
                 mainNav.removeClass('background-blue')
             }
         }
